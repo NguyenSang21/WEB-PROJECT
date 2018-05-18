c614f8c09214e5bcdc3367158ce102211d43fe41
NguyenSang21 <38211916+NguyenSang21@users.noreply.github.com>
2018-05-18 10:55:45 +0700

    Update API

--- API/models/sqlConnector.js
+++ API/models/sqlConnector.js
@@ -2,10 +2,10 @@ var mysql = require('mysql');
 // Initialize pool
 var pool      =    mysql.createPool({
     connectionLimit : 10,
-    host     : '127.0.0.1',
-    user     : 'root',
-    password : '',
-    database : 'qlbhdt',
+    host     : 'us-cdbr-iron-east-04.cleardb.net',
+    user     : 'bb8a4998b6f1d',
+    password : 'e1a0e7ad',
+    database : 'heroku_30df347d2951746',
     debug    :  false
 });
 module.exports = pool;
